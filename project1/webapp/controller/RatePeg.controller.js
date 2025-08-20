80f11042d713ce7109037dfcee7aa7dd9593b759
SimRobert <simrobert03@yahoo.com>
2025-08-20 15:20:58 +0300

    ratepeg done

--- project1/webapp/controller/RatePeg.controller.js
+++ project1/webapp/controller/RatePeg.controller.js
@@ -6,13 +6,40 @@ sap.ui.define([
   "use strict";
 
   return BaseController.extend("project1.controller.360Fb", {
-    onInit() {
+    onInit: function () {
+      // cream un model gol pentru view
       var oModel = new sap.ui.model.json.JSONModel({
-        ExpertiseRating: "",  // cheia selectata
-        ExpertiseComment: ""  // comentariu
+        FB_ID: "",
+        SelectedPeg: {}
       });
       this.getView().setModel(oModel);
-    },
+
+      this.getOwnerComponent().getRouter()
+        .getRoute("RouteRatePeg")
+        .attachPatternMatched(this._onObjectMatched, this);
+    }
+    ,
+
+    _onObjectMatched: function (oEvent) {
+      const sFbId = oEvent.getParameter("arguments").fbId;
+      console.log("Am primit FB_ID:", sFbId);
+
+      const oModel = this.getView().getModel();
+      oModel.setProperty("/FB_ID", sFbId);
+
+      // daca vrei toate datele PEG-ului selectat
+      const oPegData = this.getOwnerComponent().getModel("pegData");
+      if (oPegData) {
+        const aPegs = oPegData.getProperty("/Pegs") || [];
+        const oSelectedPeg = aPegs.find(p => p.FB_ID === sFbId);
+        if (oSelectedPeg) {
+          oModel.setProperty("/SelectedPeg", oSelectedPeg);
+          console.log("PEG selectat:", oSelectedPeg);
+        }
+      }
+    }
+
+    ,
     getRouter() {
       return sap.ui.core.UIComponent.getRouterFor(this);
     },
@@ -29,34 +56,46 @@ sap.ui.define([
       }
     },
     onSaveChanges: function () {
-      var oView = this.getView();
+      const oView = this.getView();
+      const oUserModel = this.getOwnerComponent().getModel("user");
+      const sEmail = oUserModel.getProperty("/USER_EMAIL");
 
-      // luam toate valorile (cheile)
-      var sTechSkills = oView.byId("technicalSkillsRating").getSelectedKey();
-      var sSoftSkills = oView.byId("softSkillsRating").getSelectedKey();
-      var sOtherSkills = oView.byId("otherSkillsRating").getSelectedKey();
-      var sExpertise = oView.byId("expertiseRating").getSelectedKey();
-      var sNetworking = oView.byId("networkingSkillsRating").getSelectedKey();
+      const oModel = oView.getModel();
+      const sFbId = oModel.getProperty("/FB_ID");
 
-      // luam comentariul
-      var sComment = oView.byId("expertiseComment").getValue();
+      // ia valorile din UI
+      const sTechRating = oView.byId("technicalSkillsRating").getSelectedKey();
+      const sSoftRating = oView.byId("softSkillsRating").getSelectedKey();
+      const sOtherRating = oView.byId("otherSkillsRating").getSelectedKey();
+      const sExpertiseRating = oView.byId("expertiseRating").getSelectedKey();
+      const sNetworkingRating = oView.byId("networkingSkillsRating").getSelectedKey();
+      const sComment = oView.byId("expertiseComment").getValue();
 
-      // pregatim mesajul
-      var sMessage =
-        "Technical Skills: " + sTechSkills + "\n" +
-        "Soft Skills: " + sSoftSkills + "\n" +
-        "Other Skills: " + sOtherSkills + "\n" +
-        "Expertise: " + sExpertise + "\n" +
-        "Networking Skills: " + sNetworking + "\n\n" +
-        "Comment: " + sComment;
+      const oODataModel = this.getOwnerComponent().getModel();
 
-      // afisam in MessageBox
-      sap.m.MessageBox.information(sMessage, {
-        title: "Saved Changes"
+      // apelam function import-ul
+      oODataModel.callFunction("/EditPEG_FI", {
+        method: "POST",
+        urlParameters: {
+          EMAIL: sEmail,
+          FB_ID: sFbId,
+          FB_COMMENT: sComment,
+          CAT_TECHNICAL: sTechRating,
+          CAT_SOFT: sSoftRating,
+          CAT_OTHER: sOtherRating,
+          CAT_EXPERTISE: sExpertiseRating,
+          CAT_NETWORK: sNetworkingRating
+        },
+        success: (oData) => {
+          sap.m.MessageBox.success("Feedback-ul a fost salvat cu succes!");
+          console.log("EditPEG_FI success:", oData);
+        },
+        error: (oError) => {
+          sap.m.MessageBox.error("A aparut o eroare la salvarea feedback-ului.");
+          console.error("EditPEG_FI error:", oError);
+        }
       });
     }
-
-
 
   });
 });
