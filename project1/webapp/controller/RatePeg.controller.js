7742586241672fe2966fb0be07d05e55978ae885
SimRobert <simrobert03@yahoo.com>
2025-08-20 12:21:17 +0300

    peg new fields

--- project1/webapp/controller/RatePeg.controller.js
+++ project1/webapp/controller/RatePeg.controller.js
@@ -1,26 +1,62 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
-  "sap/ui/core/routing/History"
-], (BaseController, History) => {
+  "sap/ui/core/routing/History",
+  "sap/m/MessageBox"
+], (BaseController, History, MessageBox) => {
   "use strict";
 
   return BaseController.extend("project1.controller.360Fb", {
-      onInit() {
-      },
-      getRouter() {
-            return sap.ui.core.UIComponent.getRouterFor(this);
-        },
-      onNavBack: function () {
-            var oHistory, sPreviousHash;
+    onInit() {
+      var oModel = new sap.ui.model.json.JSONModel({
+        ExpertiseRating: "",  // cheia selectata
+        ExpertiseComment: ""  // comentariu
+      });
+      this.getView().setModel(oModel);
+    },
+    getRouter() {
+      return sap.ui.core.UIComponent.getRouterFor(this);
+    },
+    onNavBack: function () {
+      var oHistory, sPreviousHash;
 
-            oHistory = History.getInstance();
-            sPreviousHash = oHistory.getPreviousHash();
+      oHistory = History.getInstance();
+      sPreviousHash = oHistory.getPreviousHash();
 
-            if (sPreviousHash !== undefined) {
-                window.history.go(-1);
-            } else {
-                this.getRouter().navTo("RouteHomePage", {}, true /*no history*/);
-            }
-        },
+      if (sPreviousHash !== undefined) {
+        window.history.go(-1);
+      } else {
+        this.getRouter().navTo("RouteHomePage", {}, true /*no history*/);
+      }
+    },
+    onSaveChanges: function () {
+      var oView = this.getView();
+
+      // luam toate valorile (cheile)
+      var sTechSkills = oView.byId("technicalSkillsRating").getSelectedKey();
+      var sSoftSkills = oView.byId("softSkillsRating").getSelectedKey();
+      var sOtherSkills = oView.byId("otherSkillsRating").getSelectedKey();
+      var sExpertise = oView.byId("expertiseRating").getSelectedKey();
+      var sNetworking = oView.byId("networkingSkillsRating").getSelectedKey();
+
+      // luam comentariul
+      var sComment = oView.byId("expertiseComment").getValue();
+
+      // pregatim mesajul
+      var sMessage =
+        "Technical Skills: " + sTechSkills + "\n" +
+        "Soft Skills: " + sSoftSkills + "\n" +
+        "Other Skills: " + sOtherSkills + "\n" +
+        "Expertise: " + sExpertise + "\n" +
+        "Networking Skills: " + sNetworking + "\n\n" +
+        "Comment: " + sComment;
+
+      // afisam in MessageBox
+      sap.m.MessageBox.information(sMessage, {
+        title: "Saved Changes"
+      });
+    }
+
+
+
   });
 });
